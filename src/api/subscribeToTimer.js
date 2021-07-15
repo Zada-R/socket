9d446c6797a35cda8516e3b441cb3d0c1f0a13e7
zhengrong55 <1183619791@qq.com>
2021-07-15 11:14:26 +0800

    feat: client

--- src/api/subscribeToTimer.js
+++ src/api/subscribeToTimer.js
@@ -1,8 +1,19 @@
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000/my-namespace");
+const socket = io("http://172.26.111.118:8000/my-namespace", {
+  transports: ["websocket"],
+});
 
 function subscribeToTimer(cb) {
+  socket.on("connect", () => {
+    console.log(socket.id);
+    setTimeout(() => {
+      // socket.close();
+      socket.disconnect();
+      console.log(socket.connected);
+      console.log(socket);
+    }, 5000);
+  });
   socket.on("timer", (timestamp) => cb(null, timestamp));
   socket.emit("subscribeToTimer", 1000);
 }
